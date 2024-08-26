692db00a39345a267d82a61f723cdad7b82deda1
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-08-25 22:16:46 -0700

    Initial Commit

---
+++ MovingBorder/MovingBorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingBorderApp.swift
+//  MovingBorder
+//
+//  Created by test on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MovingBorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
