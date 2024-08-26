f5941338c4b36531f0aa9205dc1e70a8aebf7f22
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-08-25 22:29:40 -0700

    init

--- MovingBorder/ContentView.swift
+++ MovingBorder/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isMovingAround = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 27)
+                .frame(width: 160, height: 54)
+                .foregroundStyle(.indigo.gradient)
+            RoundedRectangle(cornerRadius: 27)
+                .strokeBorder(style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round, dash: [40, 400], dashPhase: isMovingAround ? 220 : -220))
+                .frame(width: 160, height: 54)
+                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.indigo, .white, .purple, .mint, .white, .orange, .indigo]), startPoint: .trailing, endPoint: .leading))
+                .shadow(radius: 2)
+            Button {
+                
+            } label: {
+                ZStack {
+                    HStack {
+                        Text("Get Started")
+                        Image(systemName: "arrow.right")
+                    }
+                    .bold()
+                }
+            }
+            .buttonStyle(.plain)
         }
-        .padding()
+        .onAppear() {
+            withAnimation(.linear.speed(0.1).repeatForever(autoreverses: false)) {
+                isMovingAround.toggle()
+            }
+        }
     }
 }
 
